2026cc659bb224a07c92181bd8cd2e73c6f03fc3
JorgeLozornio <jlozornio321@accitesz.com>
2021-02-28 20:08:05 -0600

    Prueba de conexion BD

--- ERP_Recursos_Humanos/src/erp_recursos_humanos/Conexion.java
+++ ERP_Recursos_Humanos/src/erp_recursos_humanos/Conexion.java
@@ -3,23 +3,32 @@ package erp_recursos_humanos;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 
 public class Conexion {
     
     Connection con;
     private static final String driver = "com.mysql.jdbc.Driver";
-    private static final String user = "root";
-    private static final String pass = "";
-    
+    private static final String user = "ufcdipekfe2lrcg5";
+    private static final String pass = "EVojeEXkDROcXtIx66xt";
+    private static final String url = "jdbc:mysql://ufcdipekfe2lrcg5:EVojeEXkDROcXtIx66xt@bpon9bafeb9cpa9jaxao-mysql.services.clever-cloud.com:3306/bpon9bafeb9cpa9jaxao";
     
     public Conexion(){
+        
+        con = null;
+        
         try{
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://bpon9bafeb9cpa9jaxao-mysql.services.clever-cloud.com/bpon9bafeb9cpa9jaxao","ufcdipekfe2lrcg5","EVojeEXkDROcXtIx66xt");
-        } catch (Exception e) {
-            System.out.println("Error: " + e);
+            Class.forName(driver);
+            con = (Connection) DriverManager.getConnection(url, user, pass);
+            if(con != null){
+                System.out.println("Conexion establecida");
+            }
+            
+        } catch (ClassNotFoundException |  SQLException e){
+            System.out.println("Error de conexion" + e);
         }
+        
     }
             
 }
--- ERP_Recursos_Humanos/src/erp_recursos_humanos/ERP_Recursos_Humanos.java
+++ ERP_Recursos_Humanos/src/erp_recursos_humanos/ERP_Recursos_Humanos.java
@@ -11,18 +11,7 @@ public class ERP_Recursos_Humanos {
     public static void main(String[] args) {
         
         Conexion cn = new Conexion();
-        Statement st;
-        ResultSet rs;
         
-        try{
-            st = cn.con.createStatement();
-            rs = st.executeQuery("show tables");
-            while (rs.next()){
-                System.out.println(rs.getRow());
-            }
-            cn.con.close();
-        } catch (Exception e){            
-        }
         
     }
     
