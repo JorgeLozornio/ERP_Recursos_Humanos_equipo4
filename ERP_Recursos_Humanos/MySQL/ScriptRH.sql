feeedd4b12d9f4b12ac295d9e1c4717d8ee4bfa2
JorgeLozornio <jlozornio321@accitesz.com>
2021-03-07 23:09:39 -0600

    Update.ScriptRH.sql

--- ERP_Recursos_Humanos/MySQL/ScriptRH.sql
+++ ERP_Recursos_Humanos/MySQL/ScriptRH.sql
@@ -1,24 +1,26 @@
+
+DROP DATABASE IF exists ERP;
 /* Crear la base de datos */
 CREATE DATABASE ERP;
 /* Uso de la base de datos */
 USE ERP;
 
 /* Creación de las tablas */
-/* Tabla "Ciudades" */
+/* Tabla "Ciudades" -- A*/
 CREATE TABLE Ciudades(
 idCiudad INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 nombre VARCHAR(20) NOT NULL,
 idEstado INT NOT NULL,
 estatus CHAR);
 
-/*Tabla Estados*/
+/*Tabla Estados -- A*/
 create table Estados(
 idEstado int not null primary key auto_increment,
 nombre varchar(60),
 siglas varchar(50),
 estatus char);
 
-/*Tabla Puestos*/
+/*Tabla Puestos -- A*/
 create table Puestos(
 idPuesto int not null primary key auto_increment,
 nombre varchar(60),
@@ -26,7 +28,7 @@ salarioMinimo float,
 salarioMaximo float,
 estatus char);
 
-/*Tabla Asistencias*/
+/*Tabla Asistencias -- A*/
 create table Asistencias(
 idAsistencia int not null primary key auto_increment,
 fecha date,
@@ -35,7 +37,7 @@ horaSalida date,
 dia varchar(10),
 idEmpleado int);
 
-/*Tabla Turnos*/
+/*Tabla Turnos -- A*/
 Create Table Turnos(
 idTurno int not null primary key auto_increment,
 nombre varchar(20) NOT NULL,
@@ -43,7 +45,7 @@ horaInicio date NOT NULL,
 horaFin date NOT NULL,
 dias varchar(30) NOT NULL);
 
-/*Tabla Departamentos*/
+/*Tabla Departamentos -- A*/
 Create Table Departamentos(
 idDepartamento int not null primary key auto_increment,
 nombre varchar(50) NOT NULL,
@@ -64,7 +66,7 @@ salarioDiario float NOT NULL,
 nss varchar(10) NOT NULL,
 diasVacaciones int NOT NULL,
 diasPermiso int NOT NULL,
-fotografia image NOT NULL,
+fotografia blob NOT NULL,
 direccion varchar(80) NOT NULL,
 colonia varchar(50) NOT NULL,
 codigoPostal varchar(5) NOT NULL,
@@ -89,7 +91,7 @@ fechaFin date NOT NULL,
 tipo char NOT NULL,
 idEmpleadoSolicita int NOT NULL,
 idEmpleadoAutoriza int NOT NULL,
-evidencia image NOT NULL,
+evidencia blob NOT NULL,
 estatus char NOT NULL,
 motivo varchar(100) NOT NULL);
 
@@ -134,7 +136,7 @@ Create Table DocumentacionEmpleado(
 idDocumento int not null primary key auto_increment,
 nombreDocumento varchar(80) NOT NULL,
 fechaEntrega date NOT NULL,
-documento image NOT NULL,
+documento blob NOT NULL,
 idEmpleado int NOT NULL);
 
 /*Tabla NominasDeducciones*/
@@ -195,4 +197,4 @@ select*from Puestos;
 
 ALTER TABLE Estados
 ADD CHECK (estatus = 'A' or estatus = 'I');
-select*from Estados; 
+show tables;
