b1c8eddcb93b0b5f26ab477602853c9dbb1d3bfe
JorgeLozornio <jlozornio321@accitesz.com>
2021-04-27 09:52:41 -0500

    Interfaz Empleados

---
+++ ERP_Recursos_Humanos/src/TablasDAO/EmpleadosDAO.java
@@ -0,0 +1,17 @@
+
+package TablasDAO;
+
+import java.sql.Connection;
+
+
+public class EmpleadosDAO {
+    
+    Connection con;
+    int i;
+    int f;
+    
+    public EmpleadosDAO(Connection c){
+        con = c;
+    }
+    
+}
