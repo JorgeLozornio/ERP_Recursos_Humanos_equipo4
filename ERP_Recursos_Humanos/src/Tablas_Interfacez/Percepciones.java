f716a3354899273b214dba45d99fbc1edc9c3d96
napo315 <61436344+napo315@users.noreply.github.com>
2021-06-10 22:56:30 -0500

    botones

--- ERP_Recursos_Humanos/src/Tablas_Interfacez/Percepciones.java
+++ ERP_Recursos_Humanos/src/Tablas_Interfacez/Percepciones.java
@@ -66,19 +66,19 @@ public class Percepciones extends javax.swing.JFrame {
         txDescripcion = new javax.swing.JTextField();
         txDiaspagar = new javax.swing.JTextField();
         jEstatus = new javax.swing.JComboBox<>();
-        jRegrasar = new javax.swing.JLabel();
-        jAgregar = new javax.swing.JLabel();
-        jActualizar = new javax.swing.JLabel();
-        jElimianar = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
-        jLabel9 = new javax.swing.JLabel();
-        jLabel10 = new javax.swing.JLabel();
         jLabel11 = new javax.swing.JLabel();
         cerrarSesion = new javax.swing.JLabel();
         btnAtras = new javax.swing.JButton();
         btnSiguiente = new javax.swing.JButton();
         barra = new javax.swing.JLabel();
+        jactu = new javax.swing.JLabel();
+        jeli = new javax.swing.JLabel();
+        jregre = new javax.swing.JLabel();
+        jAgre = new javax.swing.JLabel();
+        jLabel13 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        jLabel14 = new javax.swing.JLabel();
+        jLabel15 = new javax.swing.JLabel();
         jLabelSombra = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -143,57 +143,6 @@ public class Percepciones extends javax.swing.JFrame {
         jEstatus.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "A", "I" }));
         getContentPane().add(jEstatus, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 350, 145, -1));
 
-        jRegrasar.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jRegrasar.setText("Regresar");
-        jRegrasar.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jRegrasarMouseClicked(evt);
-            }
-        });
-        getContentPane().add(jRegrasar, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 420, 60, 20));
-
-        jAgregar.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jAgregar.setForeground(new java.awt.Color(255, 255, 255));
-        jAgregar.setText("Agregar");
-        jAgregar.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jAgregarMouseClicked(evt);
-            }
-        });
-        getContentPane().add(jAgregar, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 410, 70, 30));
-
-        jActualizar.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jActualizar.setForeground(new java.awt.Color(255, 255, 255));
-        jActualizar.setText("Actualizar");
-        jActualizar.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jActualizarMouseClicked(evt);
-            }
-        });
-        getContentPane().add(jActualizar, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 410, -1, 30));
-
-        jElimianar.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jElimianar.setForeground(new java.awt.Color(255, 255, 255));
-        jElimianar.setText("Eliminar");
-        jElimianar.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jElimianarMouseClicked(evt);
-            }
-        });
-        getContentPane().add(jElimianar, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 410, -1, 30));
-
-        jLabel7.setIcon(new javax.swing.ImageIcon("C:\\Users\\napoleon\\Documents\\GitHub\\ERP_Recursos_Humanos_equipo4\\ERP_Recursos_Humanos\\src\\assets\\fondoBotonV.png")); // NOI18N
-        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 410, -1, -1));
-
-        jLabel8.setIcon(new javax.swing.ImageIcon("C:\\Users\\napoleon\\Documents\\GitHub\\ERP_Recursos_Humanos_equipo4\\ERP_Recursos_Humanos\\src\\assets\\fondoBotonRojo.png")); // NOI18N
-        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 410, -1, -1));
-
-        jLabel9.setIcon(new javax.swing.ImageIcon("C:\\Users\\napoleon\\Documents\\GitHub\\ERP_Recursos_Humanos_equipo4\\ERP_Recursos_Humanos\\src\\assets\\fondoBotonAzul.png")); // NOI18N
-        getContentPane().add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 410, -1, -1));
-
-        jLabel10.setIcon(new javax.swing.ImageIcon("C:\\Users\\napoleon\\Documents\\GitHub\\ERP_Recursos_Humanos_equipo4\\ERP_Recursos_Humanos\\src\\assets\\fondoBotonRosa.png")); // NOI18N
-        getContentPane().add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 410, 80, 40));
-
         jLabel11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/Cruz.png"))); // NOI18N
         jLabel11.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -247,71 +196,59 @@ public class Percepciones extends javax.swing.JFrame {
         });
         getContentPane().add(barra, new org.netbeans.lib.awtextra.AbsoluteConstraints(-180, 0, 1080, -1));
 
+        jactu.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jactu.setText("Actualizar");
+        jactu.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jactuMouseClicked(evt);
+            }
+        });
+        getContentPane().add(jactu, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 400, 60, 30));
+
+        jeli.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jeli.setText("Eliminar");
+        jeli.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jeliMouseClicked(evt);
+            }
+        });
+        getContentPane().add(jeli, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 400, -1, 30));
+
+        jregre.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jregre.setText("Regresar");
+        jregre.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jregreMouseClicked(evt);
+            }
+        });
+        getContentPane().add(jregre, new org.netbeans.lib.awtextra.AbsoluteConstraints(594, 400, 60, 30));
+
+        jAgre.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        jAgre.setText("Agregar");
+        jAgre.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jAgreMouseClicked(evt);
+            }
+        });
+        getContentPane().add(jAgre, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 400, -1, 30));
+
+        jLabel13.setIcon(new javax.swing.ImageIcon("C:\\Users\\napoleon\\Documents\\GitHub\\ERP_Recursos_Humanos_equipo4\\ERP_Recursos_Humanos\\src\\assets\\fondoBotonAzul.png")); // NOI18N
+        getContentPane().add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 400, -1, -1));
+
+        jLabel12.setIcon(new javax.swing.ImageIcon("C:\\Users\\napoleon\\Documents\\GitHub\\ERP_Recursos_Humanos_equipo4\\ERP_Recursos_Humanos\\src\\assets\\fondoBotonRosa.png")); // NOI18N
+        getContentPane().add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 400, -1, -1));
+
+        jLabel14.setIcon(new javax.swing.ImageIcon("C:\\Users\\napoleon\\Documents\\GitHub\\ERP_Recursos_Humanos_equipo4\\ERP_Recursos_Humanos\\src\\assets\\fondoBotonRojo.png")); // NOI18N
+        getContentPane().add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 400, -1, -1));
+
+        jLabel15.setIcon(new javax.swing.ImageIcon("C:\\Users\\napoleon\\Documents\\GitHub\\ERP_Recursos_Humanos_equipo4\\ERP_Recursos_Humanos\\src\\assets\\fondoBotonV.png")); // NOI18N
+        getContentPane().add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 400, -1, -1));
+
         jLabelSombra.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/SombraLogin.png"))); // NOI18N
         getContentPane().add(jLabelSombra, new org.netbeans.lib.awtextra.AbsoluteConstraints(-90, 0, 470, 580));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jAgregarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jAgregarMouseClicked
-        int num = Integer.parseInt(txDiaspagar.getText()) ;   
-        if (txNombre.getText().isEmpty() || txDescripcion.getText().isEmpty() || txDiaspagar.getText().isEmpty() || jEstatus.getSelectedIndex() == -1) {
-            JOptionPane.showMessageDialog(null, "Error: No dejes campos vacios");
-        } else if (num <= 0||num > 30){
-            JOptionPane.showMessageDialog(null, "Debes de introducir un valor dentro del rango permitido");
-        }else {
-            int seleccionEstatus = jEstatus.getSelectedIndex();
-             modelo_percepcion per = new modelo_percepcion();
-                per.setNombre(txNombre.getText());
-                per.setDescripcion(txDescripcion.getText());
-                per.setDiasPagar(Integer.parseInt(txDiaspagar.getText()));
-                per.setEstatus(jEstatus.getItemAt(seleccionEstatus).toString());
-            t.insertar(per);
-            limpiar();
-            // Manda a llamar el metodo: consultaDatos()
-            tableP.setModel(t.mostrarDat(inicio, fin));
-        } 
-        
-    }//GEN-LAST:event_jAgregarMouseClicked
-
-    private void jRegrasarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jRegrasarMouseClicked
-        MenuTablas m = new MenuTablas(con, us);
-        m.setVisible(true);
-        this.setVisible(false);
-        
-    }//GEN-LAST:event_jRegrasarMouseClicked
-
-    private void jActualizarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jActualizarMouseClicked
-       if (txNombre.getText().isEmpty() || txDescripcion.getText().isEmpty() || txDiaspagar.getText().isEmpty() || jEstatus.getSelectedIndex() == -1) {
-            JOptionPane.showMessageDialog(null, "Error: Debes llenar todos los campos");
-        } else {
-        int seleccionEstatus = jEstatus.getSelectedIndex();
-        int fila = tableP.getSelectedRow();
-        Integer idP = Integer.parseInt(tableP.getValueAt(tableP.getSelectedRow(), 0).toString());
-        modelo_percepcion per = new modelo_percepcion();
-        per.setIdPercep(idP);
-        per.setDescripcion(txDescripcion.getText());
-        per.setNombre(txNombre.getText());
-        per.setDiasPagar(Integer.parseInt(txDiaspagar.getText()));
-        per.setEstatus(jEstatus.getItemAt(seleccionEstatus).toString());
-        t.actualizar(per);
-        tableP.setModel(t.mostrarDat(inicio,fin));
-        limpiar();
-       }
-    }//GEN-LAST:event_jActualizarMouseClicked
-
-    private void jElimianarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jElimianarMouseClicked
-        if (txNombre.getText().isEmpty() || txDescripcion.getText().isEmpty() || txDiaspagar.getText().isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Error: Debes de seleccionar el valor a eliminar");
-        } else {
-            int filaSeleccionada = tableP.getSelectedRow();
-            String id = "" + tableP.getValueAt(filaSeleccionada, 0);
-            t.eliminar(id);
-            tableP.setModel(t.mostrarDat(inicio, fin));
-            limpiar();
-        }
-        
-    }//GEN-LAST:event_jElimianarMouseClicked
 
     private void txBuscarKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txBuscarKeyReleased
         consultaIndividual(txBuscar.getText());
@@ -380,6 +317,65 @@ int xx, xy;
         this.dispose();
         System.exit(0);
     }//GEN-LAST:event_jLabel11MouseClicked
+
+    private void jAgreMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jAgreMouseClicked
+                int num = Integer.parseInt(txDiaspagar.getText()) ;   
+        if (txNombre.getText().isEmpty() || txDescripcion.getText().isEmpty() || txDiaspagar.getText().isEmpty() || jEstatus.getSelectedIndex() == -1) {
+            JOptionPane.showMessageDialog(null, "Error: No dejes campos vacios");
+        } else if (num <= 0||num > 30){
+            JOptionPane.showMessageDialog(null, "Debes de introducir un valor dentro del rango permitido");
+        }else {
+            int seleccionEstatus = jEstatus.getSelectedIndex();
+             modelo_percepcion per = new modelo_percepcion();
+                per.setNombre(txNombre.getText());
+                per.setDescripcion(txDescripcion.getText());
+                per.setDiasPagar(Integer.parseInt(txDiaspagar.getText()));
+                per.setEstatus(jEstatus.getItemAt(seleccionEstatus).toString());
+            t.insertar(per);
+            limpiar();
+            // Manda a llamar el metodo: consultaDatos()
+            tableP.setModel(t.mostrarDat(inicio, fin));
+        } 
+        
+    }//GEN-LAST:event_jAgreMouseClicked
+
+    private void jeliMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jeliMouseClicked
+                if (txNombre.getText().isEmpty() || txDescripcion.getText().isEmpty() || txDiaspagar.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Error: Debes de seleccionar el valor a eliminar");
+        } else {
+            int filaSeleccionada = tableP.getSelectedRow();
+            String id = "" + tableP.getValueAt(filaSeleccionada, 0);
+            t.eliminar(id);
+            tableP.setModel(t.mostrarDat(inicio, fin));
+            limpiar();
+        }
+        
+    }//GEN-LAST:event_jeliMouseClicked
+
+    private void jactuMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jactuMouseClicked
+         if (txNombre.getText().isEmpty() || txDescripcion.getText().isEmpty() || txDiaspagar.getText().isEmpty() || jEstatus.getSelectedIndex() == -1) {
+            JOptionPane.showMessageDialog(null, "Error: Debes llenar todos los campos");
+        } else {
+        int seleccionEstatus = jEstatus.getSelectedIndex();
+        int fila = tableP.getSelectedRow();
+        Integer idP = Integer.parseInt(tableP.getValueAt(tableP.getSelectedRow(), 0).toString());
+        modelo_percepcion per = new modelo_percepcion();
+        per.setIdPercep(idP);
+        per.setDescripcion(txDescripcion.getText());
+        per.setNombre(txNombre.getText());
+        per.setDiasPagar(Integer.parseInt(txDiaspagar.getText()));
+        per.setEstatus(jEstatus.getItemAt(seleccionEstatus).toString());
+        t.actualizar(per);
+        tableP.setModel(t.mostrarDat(inicio,fin));
+        limpiar();
+       }
+    }//GEN-LAST:event_jactuMouseClicked
+
+    private void jregreMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jregreMouseClicked
+                MenuTablas m = new MenuTablas(con, us);
+        m.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_jregreMouseClicked
 
     public void limpiar() {
         txNombre.setText("");
@@ -451,24 +447,24 @@ int xx, xy;
     private javax.swing.JButton btnAtras;
     private javax.swing.JButton btnSiguiente;
     private javax.swing.JLabel cerrarSesion;
-    private javax.swing.JLabel jActualizar;
-    private javax.swing.JLabel jAgregar;
-    private javax.swing.JLabel jElimianar;
+    private javax.swing.JLabel jAgre;
     private javax.swing.JComboBox<String> jEstatus;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel15;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JLabel jLabel9;
     private javax.swing.JLabel jLabelSombra;
-    private javax.swing.JLabel jRegrasar;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel jactu;
+    private javax.swing.JLabel jeli;
+    private javax.swing.JLabel jregre;
     private javax.swing.JLabel lblReloj;
     private javax.swing.JTable tableP;
     private javax.swing.JTextField txBuscar;
